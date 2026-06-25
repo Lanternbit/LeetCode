1cfd3c3d8ddef0ea9701c946764485443c62ae49
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2026-06-26 01:01:48 +0900

    Time: 2 ms (72.38%), Space: 14.9 MB (37.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> hm;
-        vector<int> ans;
 
         for (int i = 0; i < nums.size(); i++) {
-            hm.insert({nums[i], i});
-
             auto it = hm.find(target - nums[i]);
-            if (it != hm.end() && i != it->second) {
-                ans.push_back(i);
-                ans.push_back(it->second);
-            }      
+            if (it != hm.end()) {
+                return {i, it->second};
+            }   
+
+            hm.insert({nums[i], i});   
         }
 
-        return ans;
+        return {};
     }
 };
